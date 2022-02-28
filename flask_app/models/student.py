1d4511e5584945c8ed47c1cf617c8962f5ade56a
Jared McCoon <jaredmccoon@protonmail.com>
2022-02-28 13:05:28 -0800

    first commit

---
+++ flask_app/models/student.py
@@ -0,0 +1,6 @@
+from flask_app.config.mysqlconnection import MySQLConnection, connectToMySQL
+import re	# the regex module
+# create a regular expression object that we'll use later   
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+from flask import flash, session
+from flask_app import bcrypt, DATABASE
---
+++ server.py
@@ -0,0 +1,7 @@
+from flask_app.controllers import admins, students, teachers 
+from flask_app import app
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
